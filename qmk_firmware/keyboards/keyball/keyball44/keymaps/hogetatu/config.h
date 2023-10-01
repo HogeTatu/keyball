5da7486f7fb32012090ab58e0ea97f98d4de2f69
HogeTatu <hararyo.tae@gmail.com>
2023-10-01 22:38:30 +0900

    Add layer.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/hogetatu/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/hogetatu/config.h
@@ -35,9 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 4
+#define AUTO_MOUSE_DEFAULT_LAYER 6
 #define AUTO_MOUSE_TIME 65535
 
 #define KEYBALL_CPI_DEFAULT 500
